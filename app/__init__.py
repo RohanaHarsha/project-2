e8f9c9aa8ad58dc94714062e71530e016364bd88
RohanaHarsha <rohanaharsha@gmail.com>
2024-06-16 09:20:52 +0530

    initialize the dlask

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 
-
-app = Flask(__name__)
-
-
-from app import routes
+def create_app():
+    app = Flask(__name__)
+    
+    # Register blueprints or routes
+    from .routes import main
+    app.register_blueprint(main)
+    
+    return app
