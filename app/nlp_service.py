ce81c12623a610bad07188455ca3ece4fd0475ca
RohanaHarsha <rohanaharsha@gmail.com>
2025-03-30 19:39:11 +0530

    full daffodil-zone project

--- app/nlp_service.py
+++
@@ -1,8 +0,0 @@
-from .nlp_module import NLPModule
-
-class NLPService:
-    def __init__(self):
-        self.nlp_module = NLPModule()
-
-    def search_properties(self, query: str):
-        return self.nlp_module.predict(query)
